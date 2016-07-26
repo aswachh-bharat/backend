f8b2fbc2be45d65e217a0ebc80d67607ed110a47
avi <hi@avi.im>
2016-07-26 19:34:24 +0530

    Add `ImageSerializer` and `ImageViewSet`

---
+++ swachhbharat/images/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from rest_framework_gis.serializers import GeometryField
+
+from .models import Image
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    file = serializers.ImageField(use_url=True)
+    location = GeometryField(required=False)
+
+    class Meta:
+        model = Image
+        fields = ('file', 'location', )
