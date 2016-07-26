c97fae43cff65b6735593cc0dcea19d192080bfc
avi <hi@avi.im>
2016-07-26 20:57:24 +0530

    Add py.test and a sample test case

---
+++ swachhbharat/images/tests/test_images_api.py
@@ -0,0 +1,10 @@
+from rest_framework.test import APITestCase
+
+
+class ImageApiTest(APITestCase):
+
+    def test_get(self):
+        assert 1 == 1
+
+    def test_post(self):
+        assert 1 == 1
