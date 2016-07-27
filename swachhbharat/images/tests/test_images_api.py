7e4d71c38076a746929e8b4ffc6e615ed836120b
Sandilya Jandhyala <jysandilya@gmail.com>
2016-07-27 14:35:47 +0530

    Rudimentary tests added

--- swachhbharat/images/tests/test_images_api.py
+++ swachhbharat/images/tests/test_images_api.py
@@ -1,10 +1,26 @@
+import tempfile
+
+from rest_framework import status
 from rest_framework.test import APITestCase
+import PIL.Image
 
 
 class ImageApiTest(APITestCase):
+    fixtures = ['image_fixtures.json']
 
     def test_get(self):
-        assert 1 == 1
+        response = self.client.get('/api/images/')
+        assert response.status_code == status.HTTP_200_OK
+        
+        # Inserted as part of the fixture
+        assert 'foo.jpg' in response.json()['results'][0]['file']
 
     def test_post(self):
-        assert 1 == 1
+        image = PIL.Image.new('RGB', (100, 100))
+        temp_file = tempfile.NamedTemporaryFile(suffix='.jpg')
+        image.save(temp_file, format='JPEG')
+        with open(temp_file.name, 'rb') as data_file:
+            response = self.client.post('/api/images/', 
+                                        {'file': data_file}, 
+                                        format='multipart')
+            assert response.status_code == status.HTTP_201_CREATED
