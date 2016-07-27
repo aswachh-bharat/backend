701e93961a032afde1b8d46591873ec40c1e2ff9
Sandilya Jandhyala <jysandilya@gmail.com>
2016-07-27 15:04:39 +0530

    Set the spatialite library settings

--- swachhbharat/settings/dev.py
+++ swachhbharat/settings/dev.py
@@ -1,3 +1,6 @@
+import sys
+import os
+
 from .common import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -6,7 +9,14 @@ SECRET_KEY = 'm2@z#2sq(v!6*6@c3r&4x)c0k-=qbu^im7f2gooc$7y@_347#m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-SPATIALITE_LIBRARY_PATH = '/usr/local/lib/mod_spatialite.dylib'
+if sys.platform == 'darwin':
+    # Setting for spatialite on OS X
+    SPATIALITE_LIBRARY_PATH = '/usr/local/lib/mod_spatialite.dylib'
+
+if os.path.exists('/usr/lib/x86_64-linux-gnu/mod_spatialite.so'):
+    # Setting for GNU/Linux systems with spatialite 4.2+
+    SPATIALITE_LIBRARY_PATH = 'mod_spatialite'
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.spatialite',
