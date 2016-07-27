7e7c968a17d83ae50d33d5887a17e38638029ba1
Sandilya Jandhyala <jysandilya@gmail.com>
2016-07-27 14:51:06 +0530

    Added tests for the remaining API actions

--- swachhbharat/images/tests/test_images_api.py
+++ swachhbharat/images/tests/test_images_api.py
@@ -24,3 +24,26 @@ class ImageApiTest(APITestCase):
                                         {'file': data_file}, 
                                         format='multipart')
             assert response.status_code == status.HTTP_201_CREATED
+
+    def test_retrieve_action(self):
+        response = self.client.get('/api/images/1/')
+        assert response.status_code == status.HTTP_200_OK
+        assert 'foo.jpg' in response.json()['file']
+
+    def test_update_action(self):
+        image = PIL.Image.new('RGB', (100, 100))
+        temp_file = tempfile.NamedTemporaryFile(suffix='.jpg')
+        image.save(temp_file, format='JPEG')
+        with open(temp_file.name, 'rb') as data_file:
+            response = self.client.put('/api/images/1/',
+                                       {'file': data_file},
+                                       format='multipart')
+            assert response.status_code == status.HTTP_200_OK
+            assert temp_file.name.split('/')[-1] in response.json()['file']
+
+    def test_delete_action(self):
+        response = self.client.delete('/api/images/1/')
+        assert response.status_code == status.HTTP_204_NO_CONTENT
+
+        response = self.client.get('/api/images/1/')
+        assert response.status_code == status.HTTP_404_NOT_FOUND
