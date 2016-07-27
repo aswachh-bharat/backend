9e9cb756843d49afb6161bd7c4fc23f9535d87a5
Sandilya Jandhyala <jysandilya@gmail.com>
2016-07-27 15:04:39 +0530

    Set the SPATIALITE_LIBRARY_PATH only on OS X

--- swachhbharat/settings/dev.py
+++ swachhbharat/settings/dev.py
@@ -1,3 +1,5 @@
+import sys
+
 from .common import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -6,7 +8,9 @@ SECRET_KEY = 'm2@z#2sq(v!6*6@c3r&4x)c0k-=qbu^im7f2gooc$7y@_347#m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-SPATIALITE_LIBRARY_PATH = '/usr/local/lib/mod_spatialite.dylib'
+if sys.platform == 'darwin':
+    SPATIALITE_LIBRARY_PATH = '/usr/local/lib/mod_spatialite.dylib'
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.spatialite',
